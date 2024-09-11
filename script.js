0c279f4477cd2c50139eaab413d1a9d3538837fd
raghad-smth <143431529+raghad-smth@users.noreply.github.com>
2024-09-11 21:38:18 +0300

    added first round of text animation

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let menueButton = document.getElementById('menu-btn');
 let navBar = document.querySelector('.small-dev-menu');
 
 
+
 navLinks.forEach(link => {
     if(link.href === currntUrl ){
         link.classList.add('current-link');
@@ -11,13 +12,49 @@ navLinks.forEach(link => {
 });
 
 
-function showPhoneMenu (){
-    navBar.style.display = 'block';
-}
-function hidePhoneMenu (){
-    navBar.style.display = 'none';
-}
+
+const aboutParg = document.querySelectorAll('.animated-p');
+
+const aboutPargObserver= new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        } else {
+            entry.target.classList.remove('animate');
+        }
+    });
+}, {
+    threshold:0.2
+});
+
+aboutParg.forEach((el) =>  aboutPargObserver.observe(el));
 
 
-menueButton.addEventListener('mouseover', showPhoneMenu);
-menueButton.addEventListener('click',  hidePhoneMenu);
+
+const observer = new IntersectionObserver((enteries) =>{
+    enteries.forEach((entry) =>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const anitmatedPosts =  document.querySelectorAll('.a-post-box');
+anitmatedPosts.forEach((el) =>  observer.observe(el));
+
+
+// function showPhoneMenu (){
+//     navBar.style.display = 'block';
+// }
+// function hidePhoneMenu (){
+//     navBar.style.display = 'none';
+// }
+
+
+// menueButton.addEventListener('mouseover', showPhoneMenu);
+// menueButton.addEventListener('click',  hidePhoneMenu);
